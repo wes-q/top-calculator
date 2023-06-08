c11f51a56b237507d715fdef920127e6c695934b
Wes <piuswesley@gmail.com>
2023-06-08 09:32:35 +0800

    Add functionality of decimal point

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
+let numberSaved = false;
+
 document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.number').forEach(numberButton => numberButton.addEventListener('click', concatNumberToDisplay));
+    document.querySelector('.calc-screen').addEventListener('input', addCommasToScreen);
+    document.getElementById("clear").addEventListener('click', () => clearScreen());
+    document.getElementById("delete").addEventListener('click', () => backspace());
+    document.getElementById("btn-decimal").addEventListener('click', () => concatDecimalToDisplay());
+});    
 
+
+function backspace () {
     const calcScreen = document.querySelector('.calc-screen');
-    const numberElements = document.querySelectorAll('.number');
-    // document.querySelector('#btn-reset').addEventListener('click', () => { refreshPage() });
-
-    numberElements.forEach(function(numberButton) {
-        numberButton.addEventListener('click', handleClick);
-    });
-
-    calcScreen.addEventListener('input', addCommasToScreen);
-    document.getElementById("clear").addEventListener('click', clearScreen);
-    // document.getElementById("clear").addEventListener('click', () => clearScreen());
-
-    function handleClick (event) {
-        const number = event.target.textContent;
-        const screenValue = calcScreen.value;
-
-        // Do not allow leading zeros
-        if (screenValue === "0") {
-            calcScreen.value = "";
-            calcScreen.value += number;
-        } else {
-            calcScreen.value += number;
-        }
+    const screenValue = calcScreen.value; // Get the current displayed value
+    let newScreenValue =  screenValue.slice(0, -1);
     
-        // Trigger the 'input' event manually to trigger function addCommasToScreen
-        const eventx = new Event('input');
-        calcScreen.dispatchEvent(eventx);
-    }
+    // Add a zero if the input is empty after a backspace.
+    newScreenValue = newScreenValue === "" ? "0" : newScreenValue;
 
-    function addCommasToScreen (event) {
+    calcScreen.value = newScreenValue; // Apply the new screenvalue to the screen
+    triggerInputEvent();
+}
 
-        let value = event.target.value;
-        value = value.replace(/,/g, '');
-        value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        event.target.value = value;
-    }
-    
-    function clearScreen (event) {
-        calcScreen.value = "0";
-    }
 
-});
+function concatNumberToDisplay (event) {
+    const calcScreen = document.querySelector('.calc-screen');
+    const number = event.target.textContent; // Get the number pressed
+    const screenValue = calcScreen.value; // Get the current displayed value
+
+    // Do not allow leading zeros
+    if (screenValue === "0") {
+        calcScreen.value = "";
+        calcScreen.value += number;
+    } else {
+        calcScreen.value += number;
+    }
+    triggerInputEvent();
+}
+
+
+function concatDecimalToDisplay () {
+    const calcScreen = document.querySelector('.calc-screen');
+    const screenValue = calcScreen.value;
+
+    // Dont allow another decimal point in the display    
+    if (screenValue.includes(".")) {
+        // trigger sound effect
+    } else {
+        calcScreen.value += ".";
+    }
+    triggerInputEvent();
+}
+
+
+function addCommasToScreen (event) {
+    let value = event.target.value;
+    value = value.replace(/,/g, '');
+    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    event.target.value = value;
+}
+
+
+function clearScreen (event) {
+    document.querySelector('.calc-screen').value = "0";
+}
+
+
+function triggerInputEvent() {
+    // Trigger the 'input' event manually to trigger function addCommasToScreen
+    const eventx = new Event('input');
+    document.querySelector('.calc-screen').dispatchEvent(eventx);
+}
